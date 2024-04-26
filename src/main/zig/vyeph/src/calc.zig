902d61d23e268f6ab27a8f201ad68a273701ea86
vyeph <verspeek.valentin@gmail.com>
2024-04-26 21:50:18 +0200

    Optimize read into buffer

--- src/main/zig/vyeph/src/calc.zig
+++ src/main/zig/vyeph/src/calc.zig
@@ -43,31 +43,38 @@ pub fn main() anyerror!void {
     var file = try std.fs.cwd().openFile(file_path, .{});
     defer file.close();
 
-    const buffer_size = 2048;
-    var line_buffer: [buffer_size]u8 = undefined;
+    const buffer_size = 4096;
+    var buffer: [buffer_size * 2]u8 = undefined;
+    @memset(&buffer, 0);
+    const line_buffer: *[buffer_size]u8 = buffer[4096..];
 
-    var buf_reader = std.io.bufferedReader(file.reader());
-    var in_stream = buf_reader.reader();
+    const file_reader = file.reader();
+    var buf_reader = std.io.BufferedReader(buffer_size, @TypeOf(file_reader)){ .unbuffered_reader = file_reader };
 
-    var counter: u64 = 0;
-    while (try in_stream.readUntilDelimiterOrEof(&line_buffer, '\n')) |line| {
-        if (counter % 50000000 == 0) {
-            std.debug.print("Line entries read : {d}/1000000000\n", .{counter});
-        }
-        var it = std.mem.splitAny(u8, line, ";");
-        const key = it.next().?;
-        const val = try std.fmt.parseFloat(f32, it.next().?);
+    var start: usize = 0;
+    while (true) {
+        const bytes_read = try buf_reader.read(line_buffer);
+        var lines = std.mem.splitAny(u8, buffer[buffer_size - start .. buffer_size + bytes_read], "\n");
+        const last_line = while (lines.next()) |line| {
+            if (line.len == 0) break "";
+            if (lines.peek() == null and bytes_read == buffer_size) break line;
+            var it = std.mem.splitAny(u8, line, ";");
+            const key = it.next().?;
+            const val = try std.fmt.parseFloat(f32, it.next().?);
 
-        if (measurements.getPtr(key)) |m| {
-            m.*.calc(val);
-        } else {
-            var station = try std.ArrayListUnmanaged(u8).initCapacity(allocator, key.len);
-            try station.appendSlice(allocator, key);
-            const measurement = Measurement.init(val);
-            try measurements.putNoClobber(allocator, station.items, measurement);
-            try stations.append(allocator, station);
-        }
-        counter += 1;
+            if (measurements.getPtr(key)) |m| {
+                m.*.calc(val);
+            } else {
+                var station = try std.ArrayListUnmanaged(u8).initCapacity(allocator, key.len);
+                try station.appendSlice(allocator, key);
+                const measurement = Measurement.init(val);
+                try measurements.putNoClobber(allocator, station.items, measurement);
+                try stations.append(allocator, station);
+            }
+        } else "";
+        if (bytes_read < buffer_size) break;
+        std.mem.copyForwards(u8, &buffer, line_buffer);
+        start = last_line.len;
     }
 
     const list = try stations.toOwnedSlice(allocator);
